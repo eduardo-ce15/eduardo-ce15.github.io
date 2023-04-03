7f094195c8481e0a5486bffe2a0c7c46a0d45dd8
eduardo-ce15 <81893920+eduardo-ce15@users.noreply.github.com>
2023-04-03 17:36:32 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function mostrarContenido() {
+    var contenido = document.getElementById("contenido");
+    if (contenido.style.display === "none") {
+      contenido.style.display = "block";
+    } else {
+      contenido.style.display = "none";
+    }
+  }
+  
